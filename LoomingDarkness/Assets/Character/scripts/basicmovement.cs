f222a18205acea6aad0a03ac2bec758de5d88de8
ViktorSveins <viktorsveins@gmail.com>
2018-12-03 14:40:25 +0000

    created a animated character with movement controls

---
+++ LoomingDarkness/Assets/Character/scripts/basicmovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicmovement : MonoBehaviour {
+	public int speed = 1;
+	public Animator animator;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+		
+		animator.SetFloat("Horizontal", move.x);
+		animator.SetFloat("Vertical", move.y);
+		animator.SetFloat("Magnitude", move.magnitude);
+
+		if(move.x < 0) {
+			animator.SetInteger("Direction", 0);
+		}
+		else if(move.x > 0) {
+			animator.SetInteger("Direction", 1);
+		}
+		else if(move.y < 0) {
+			animator.SetInteger("Direction", 2);
+		}
+		else if(move.y > 0) {
+			animator.SetInteger("Direction", 3);
+		}
+
+		transform.position = transform.position + move * Time.deltaTime * speed;
+	}
+}
