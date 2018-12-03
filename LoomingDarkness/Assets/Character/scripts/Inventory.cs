7e8e09a05629e2bbffc8b587252baaf4824c2a74
ViktorSveins <viktorsveins@gmail.com>
2018-12-03 18:16:03 +0000

    Added inventory to character

---
+++ LoomingDarkness/Assets/Character/scripts/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	public GameObject[] inventory = new GameObject[6];
+
+	public void AddItem(GameObject item) {
+		bool wasAdded = false;
+		for(int i = 0; i < inventory.Length; i++) {
+			if(inventory[i] == null) {
+				inventory[i] = item;
+				Debug.Log(item.name + " was added to the inventory");
+				wasAdded = true;
+				break;
+			}
+		}
+
+		if(wasAdded == false) {
+			Debug.Log(item.name + " was NOT added to the inventory");
+		}
+	}
+}
