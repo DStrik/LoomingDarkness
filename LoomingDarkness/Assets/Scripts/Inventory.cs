2877e2021fa4cca48109b747b33d59a79f2d9096
ViktorSveins <viktorsveins@gmail.com>
2018-12-04 00:11:12 +0000

    torch can now be picked up

--- LoomingDarkness/Assets/Scripts/Inventory.cs
+++ LoomingDarkness/Assets/Scripts/Inventory.cs
@@ -6,19 +6,20 @@ public class Inventory : MonoBehaviour {
 
 	public GameObject[] inventory = new GameObject[6];
 
-	public void AddItem(GameObject item) {
+	public bool AddItem(GameObject item) {
 		bool wasAdded = false;
 		for(int i = 0; i < inventory.Length; i++) {
 			if(inventory[i] == null) {
 				inventory[i] = item;
 				Debug.Log(item.name + " was added to the inventory");
 				wasAdded = true;
-				break;
+				return true;
 			}
 		}
 
 		if(wasAdded == false) {
 			Debug.Log(item.name + " was NOT added to the inventory");
 		}
+		return false;
 	}
 }
---
+++ LoomingDarkness/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+	public GameObject currInterObj = null;
+	public InteractableObject currInterObjScript = null;
+	public Inventory inventory;
+
+	void Update() {
+		if(Input.GetButtonDown("Interact") && currInterObj) {
+			if(currInterObjScript.storeable) {
+				if(inventory.AddItem(currInterObj)) {
+					currInterObj.SendMessage("setInactive");
+					currInterObj = null;
+					currInterObjScript = null;
+				}
+			}
+		}	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.CompareTag("interObj")) {
+			Debug.Log(other.name + " interactable collision");
+			currInterObj = other.gameObject;
+			currInterObjScript = currInterObj.GetComponent<InteractableObject>();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.CompareTag("interObj")) {
+			if(other.gameObject == currInterObj) {
+				currInterObj = null;
+				currInterObjScript = null;
+			}
+		}
+	}
+}
