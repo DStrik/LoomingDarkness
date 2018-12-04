bc80e9d0706e1f6837f917e55a0865ed8806e8a8
DStrik <dannimb@gmail.com>
2018-12-04 16:38:11 +0000

    created a health system with a bar

---
+++ LoomingDarkness/Assets/Scripts/HealthHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthHandler : MonoBehaviour {
+
+	public HealthBar healthBar;
+	private HealthSystem healthSystem;
+	public float secDelay = 0;
+	public float delayPeriod = 1;
+
+	// Use this for initialization
+	void Start () {
+		healthSystem = new HealthSystem(100);
+
+		healthBar.Setup(healthSystem);
+	}
+
+	void Update() {
+		if(Time.time > secDelay) {
+			secDelay += delayPeriod;
+			healthSystem.damage(1);
+		}
+	}
+	
+}
---
+++ LoomingDarkness/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public class HealthSystem {
+
+	public event EventHandler OnHealthChange;
+	private int health;
+	private int healthMax;
+
+	public HealthSystem(int healthMax) {
+		this.healthMax = healthMax;
+		health = healthMax;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public float getHealthPercentage() {
+		return (float) health / healthMax;
+	}
+
+	public void damage(int damageAmount) {
+		health -= damageAmount;
+
+		if (health < 0) {
+			health = 0;
+		}
+
+		if (OnHealthChange != null) {
+			OnHealthChange(this, EventArgs.Empty);
+		}
+	}
+
+	public void heal(int healAmount) {
+		health += healAmount;
+
+		if (health > healthMax) {
+			health = healthMax;
+		}
+
+		if (OnHealthChange != null) {
+			OnHealthChange(this, EventArgs.Empty);
+		}
+	}
+}
