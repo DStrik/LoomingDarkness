00b3cd213660f6f555ef831e02a3412fc55f90ce
JonniPolar <jonj16@ru.is>
2018-12-05 12:57:58 +0000

    lightn handler and lightsystem added, prefab fixed

---
+++ LoomingDarkness/Assets/Scripts/LightHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightHandler : MonoBehaviour {
+
+	private LightSystem lightSystem;
+	public GameObject light;
+	private float secDelay = 0;	
+	public float delayPeriod = 1;
+	public float lightDepletion = (float) 0.2;
+
+
+	// Use this for initialization
+	void Start () {
+		lightSystem = new LightSystem(20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lightSystem.getLight() > 0) {
+			if(Time.time > secDelay) {
+				secDelay += delayPeriod;
+				lightSystem.deplete(lightDepletion);
+				light.transform.localScale = new Vector3(lightSystem.getLight(),lightSystem.getLight(),1);
+				// reduce torch health
+			}
+		}
+	}
+}
---
+++ LoomingDarkness/Assets/Scripts/LightSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class LightSystem {
+
+	public event EventHandler OnLightChange;
+	private float light;
+	private float maxlight;
+	// Use this for initialization
+
+	public LightSystem(float maxlight){
+		this.maxlight = maxlight;
+		light = this.maxlight;
+	}
+
+	public float getLight() {
+		return light;
+	}
+
+	public void deplete(float deplete) {
+		light -= deplete;
+
+		if(light < 0) {
+			light = 0;
+		}
+
+		if(OnLightChange != null) {
+			OnLightChange(this, EventArgs.Empty);
+		}
+	}
+}
