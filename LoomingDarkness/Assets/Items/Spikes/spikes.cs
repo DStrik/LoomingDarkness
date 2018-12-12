110304055f8dca79bc22c99895bd86392b328a17
ViktorSveins <viktorsveins@gmail.com>
2018-12-12 16:01:04 +0000

    red interval spikes up and running

--- LoomingDarkness/Assets/Items/Spikes/spikes.cs
+++ LoomingDarkness/Assets/Items/Spikes/spikes.cs
@@ -7,12 +7,20 @@ public class spikes : MonoBehaviour {
 	public bool up = false;
 	public Animator animator;
 	public float timeInterval = 0;
+	public float startDelay = 2;
 
 	private void Start() {
 		animator.SetBool("Up", up);
 
 		if(timeInterval != 0) {
-        	InvokeRepeating("onChange", 2.0f, timeInterval);
+        	InvokeRepeating("OnSwitch", startDelay, timeInterval);
+		}
+
+		if(!up) {
+			BoxCollider2D[] colliders = GetComponents<BoxCollider2D>();
+			foreach(BoxCollider2D c in colliders) {
+				c.enabled = !c.enabled;
+			}
 		}
 	}
 	public void OnSwitch() {
